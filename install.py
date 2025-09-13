f7486674362d804c2e92f01f15cc7b96c4adebc3
Spencer Karjala <spencerkarjala@gmail.com>
2025-09-12 20:26:31 -0700

    Move from installer shell script to python script

---
+++ install.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+def install_all():
+    print("Installing all components...")
+
+def install_targets(targets):
+    for target in targets:
+        print(f"Installing {target}...")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: ./install.py <all|target1 target2 ...>")
+        sys.exit(1)
+
+    if sys.argv[1] == 'all':
+        install_all()
+    else:
+        install_targets(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
